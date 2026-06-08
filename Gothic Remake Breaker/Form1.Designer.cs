801418d9c91468d7c0114f21168f0d6e7ff010c7
Ivancheg8 <pavlevan@yandex.ru>
2026-06-08 20:39:51 +0300

    works

--- Gothic Remake Breaker/Form1.Designer.cs
+++ Gothic Remake Breaker/Form1.Designer.cs
@@ -22,7 +22,248 @@
 
         #region Windows Form Designer generated code
 
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.labelPlateCount = new System.Windows.Forms.Label();
+            this.panelPlateCountChecks = new System.Windows.Forms.FlowLayoutPanel();
+            this.chkPlateCount2 = new System.Windows.Forms.CheckBox();
+            this.chkPlateCount3 = new System.Windows.Forms.CheckBox();
+            this.chkPlateCount4 = new System.Windows.Forms.CheckBox();
+            this.chkPlateCount5 = new System.Windows.Forms.CheckBox();
+            this.chkPlateCount6 = new System.Windows.Forms.CheckBox();
+            this.chkPlateCount7 = new System.Windows.Forms.CheckBox();
+            this.labelStartPositions = new System.Windows.Forms.Label();
+            this.panelPlates = new System.Windows.Forms.FlowLayoutPanel();
+            this.labelEffectsTitle = new System.Windows.Forms.Label();
+            this.panelEffects = new System.Windows.Forms.FlowLayoutPanel();
+            this.buttonSolve = new System.Windows.Forms.Button();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.panelPlateCountChecks.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelPlateCount
+            // 
+            this.labelPlateCount.AutoSize = true;
+            this.labelPlateCount.Location = new System.Drawing.Point(12, 15);
+            this.labelPlateCount.Name = "labelPlateCount";
+            this.labelPlateCount.Size = new System.Drawing.Size(113, 13);
+            this.labelPlateCount.TabIndex = 0;
+            this.labelPlateCount.Text = "Количество пластин:";
+            // 
+            // panelPlateCountChecks
+            // 
+            this.panelPlateCountChecks.Controls.Add(this.chkPlateCount2);
+            this.panelPlateCountChecks.Controls.Add(this.chkPlateCount3);
+            this.panelPlateCountChecks.Controls.Add(this.chkPlateCount4);
+            this.panelPlateCountChecks.Controls.Add(this.chkPlateCount5);
+            this.panelPlateCountChecks.Controls.Add(this.chkPlateCount6);
+            this.panelPlateCountChecks.Controls.Add(this.chkPlateCount7);
+            this.panelPlateCountChecks.Location = new System.Drawing.Point(131, 10);
+            this.panelPlateCountChecks.Name = "panelPlateCountChecks";
+            this.panelPlateCountChecks.Size = new System.Drawing.Size(230, 25);
+            this.panelPlateCountChecks.TabIndex = 1;
+            // 
+            // chkPlateCount2
+            // 
+            this.chkPlateCount2.AutoSize = true;
+            this.chkPlateCount2.Checked = true;
+            this.chkPlateCount2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkPlateCount2.Location = new System.Drawing.Point(3, 3);
+            this.chkPlateCount2.Name = "chkPlateCount2";
+            this.chkPlateCount2.Size = new System.Drawing.Size(32, 17);
+            this.chkPlateCount2.TabIndex = 0;
+            this.chkPlateCount2.Tag = "2";
+            this.chkPlateCount2.Text = "2";
+            this.chkPlateCount2.UseVisualStyleBackColor = true;
+            this.chkPlateCount2.CheckedChanged += new System.EventHandler(this.plateCountCheck_CheckedChanged);
+            // 
+            // chkPlateCount3
+            // 
+            this.chkPlateCount3.AutoSize = true;
+            this.chkPlateCount3.Location = new System.Drawing.Point(41, 3);
+            this.chkPlateCount3.Name = "chkPlateCount3";
+            this.chkPlateCount3.Size = new System.Drawing.Size(32, 17);
+            this.chkPlateCount3.TabIndex = 1;
+            this.chkPlateCount3.Tag = "3";
+            this.chkPlateCount3.Text = "3";
+            this.chkPlateCount3.UseVisualStyleBackColor = true;
+            this.chkPlateCount3.CheckedChanged += new System.EventHandler(this.plateCountCheck_CheckedChanged);
+            // 
+            // chkPlateCount4
+            // 
+            this.chkPlateCount4.AutoSize = true;
+            this.chkPlateCount4.Location = new System.Drawing.Point(79, 3);
+            this.chkPlateCount4.Name = "chkPlateCount4";
+            this.chkPlateCount4.Size = new System.Drawing.Size(32, 17);
+            this.chkPlateCount4.TabIndex = 2;
+            this.chkPlateCount4.Tag = "4";
+            this.chkPlateCount4.Text = "4";
+            this.chkPlateCount4.UseVisualStyleBackColor = true;
+            this.chkPlateCount4.CheckedChanged += new System.EventHandler(this.plateCountCheck_CheckedChanged);
+            // 
+            // chkPlateCount5
+            // 
+            this.chkPlateCount5.AutoSize = true;
+            this.chkPlateCount5.Location = new System.Drawing.Point(117, 3);
+            this.chkPlateCount5.Name = "chkPlateCount5";
+            this.chkPlateCount5.Size = new System.Drawing.Size(32, 17);
+            this.chkPlateCount5.TabIndex = 3;
+            this.chkPlateCount5.Tag = "5";
+            this.chkPlateCount5.Text = "5";
+            this.chkPlateCount5.UseVisualStyleBackColor = true;
+            this.chkPlateCount5.CheckedChanged += new System.EventHandler(this.plateCountCheck_CheckedChanged);
+            // 
+            // chkPlateCount6
+            // 
+            this.chkPlateCount6.AutoSize = true;
+            this.chkPlateCount6.Location = new System.Drawing.Point(155, 3);
+            this.chkPlateCount6.Name = "chkPlateCount6";
+            this.chkPlateCount6.Size = new System.Drawing.Size(32, 17);
+            this.chkPlateCount6.TabIndex = 4;
+            this.chkPlateCount6.Tag = "6";
+            this.chkPlateCount6.Text = "6";
+            this.chkPlateCount6.UseVisualStyleBackColor = true;
+            this.chkPlateCount6.CheckedChanged += new System.EventHandler(this.plateCountCheck_CheckedChanged);
+            // 
+            // chkPlateCount7
+            // 
+            this.chkPlateCount7.AutoSize = true;
+            this.chkPlateCount7.Location = new System.Drawing.Point(193, 3);
+            this.chkPlateCount7.Name = "chkPlateCount7";
+            this.chkPlateCount7.Size = new System.Drawing.Size(32, 17);
+            this.chkPlateCount7.TabIndex = 5;
+            this.chkPlateCount7.Tag = "7";
+            this.chkPlateCount7.Text = "7";
+            this.chkPlateCount7.UseVisualStyleBackColor = true;
+            this.chkPlateCount7.CheckedChanged += new System.EventHandler(this.plateCountCheck_CheckedChanged);
+            // 
+            // labelStartPositions
+            // 
+            this.labelStartPositions.AutoSize = true;
+            this.labelStartPositions.Location = new System.Drawing.Point(12, 50);
+            this.labelStartPositions.Name = "labelStartPositions";
+            this.labelStartPositions.Size = new System.Drawing.Size(278, 13);
+            this.labelStartPositions.TabIndex = 2;
+            this.labelStartPositions.Text = "Выберите начальную позицию для каждой пластины:";
+            // 
+            // panelPlates
+            // 
+            this.panelPlates.AutoScroll = true;
+            this.panelPlates.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.panelPlates.Location = new System.Drawing.Point(5, 10);
+            this.panelPlates.Name = "panelPlates";
+            this.panelPlates.Size = new System.Drawing.Size(341, 248);
+            this.panelPlates.TabIndex = 3;
+            // 
+            // labelEffectsTitle
+            // 
+            this.labelEffectsTitle.AutoSize = true;
+            this.labelEffectsTitle.Location = new System.Drawing.Point(390, 50);
+            this.labelEffectsTitle.Name = "labelEffectsTitle";
+            this.labelEffectsTitle.Size = new System.Drawing.Size(392, 26);
+            this.labelEffectsTitle.TabIndex = 4;
+            this.labelEffectsTitle.Text = "Эффекты:\r\n← левые чекбоксы = Opposite (влево), правые чекбоксы → = Same (вправо)";
+            // 
+            // panelEffects
+            // 
+            this.panelEffects.AutoScroll = true;
+            this.panelEffects.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.panelEffects.Location = new System.Drawing.Point(4, 11);
+            this.panelEffects.Name = "panelEffects";
+            this.panelEffects.Size = new System.Drawing.Size(621, 247);
+            this.panelEffects.TabIndex = 5;
+            // 
+            // buttonSolve
+            // 
+            this.buttonSolve.Location = new System.Drawing.Point(15, 356);
+            this.buttonSolve.Name = "buttonSolve";
+            this.buttonSolve.Size = new System.Drawing.Size(75, 23);
+            this.buttonSolve.TabIndex = 6;
+            this.buttonSolve.Text = "Решить";
+            this.buttonSolve.UseVisualStyleBackColor = true;
+            this.buttonSolve.Click += new System.EventHandler(this.buttonSolve_Click);
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Location = new System.Drawing.Point(15, 386);
+            this.textBoxResult.Multiline = true;
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.ReadOnly = true;
+            this.textBoxResult.Size = new System.Drawing.Size(226, 217);
+            this.textBoxResult.TabIndex = 8;
+            this.textBoxResult.WordWrap = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.groupBox1.Controls.Add(this.panelEffects);
+            this.groupBox1.Location = new System.Drawing.Point(370, 79);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(630, 262);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.groupBox2.Controls.Add(this.panelPlates);
+            this.groupBox2.Location = new System.Drawing.Point(15, 79);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(349, 262);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1013, 615);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.buttonSolve);
+            this.Controls.Add(this.labelEffectsTitle);
+            this.Controls.Add(this.labelStartPositions);
+            this.Controls.Add(this.panelPlateCountChecks);
+            this.Controls.Add(this.labelPlateCount);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Gothic Remake Breaker";
+            this.panelPlateCountChecks.ResumeLayout(false);
+            this.panelPlateCountChecks.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
 
         #endregion
+
+        private System.Windows.Forms.Label labelPlateCount;
+        private System.Windows.Forms.FlowLayoutPanel panelPlateCountChecks;
+        private System.Windows.Forms.CheckBox chkPlateCount2;
+        private System.Windows.Forms.CheckBox chkPlateCount3;
+        private System.Windows.Forms.CheckBox chkPlateCount4;
+        private System.Windows.Forms.CheckBox chkPlateCount5;
+        private System.Windows.Forms.CheckBox chkPlateCount6;
+        private System.Windows.Forms.CheckBox chkPlateCount7;
+        private System.Windows.Forms.Label labelStartPositions;
+        private System.Windows.Forms.FlowLayoutPanel panelPlates;
+        private System.Windows.Forms.Label labelEffectsTitle;
+        private System.Windows.Forms.FlowLayoutPanel panelEffects;
+        private System.Windows.Forms.Button buttonSolve;
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
--- Gothic Remake Breaker/Form1.cs
+++ Gothic Remake Breaker/Form1.cs
@@ -1,11 +1,331 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 
 namespace Gothic_Remake_Breaker
 {
+    public partial class Form1 : Form
+    {
+        // Кэш контролов — создаются один раз при загрузке для макс. количества пластин
+        private FlowLayoutPanel[] _platePanels = new FlowLayoutPanel[7];
+
+        // Кэш строк эффектов (TableLayoutPanel) — по одной на каждый источник 0..6
+        private TableLayoutPanel[] _effectRows = new TableLayoutPanel[7];
+
+        // Кэш чекбоксов эффектов: [src, tgt] → Same / Opposite
+        private CheckBox[,] _sameCheckBoxes = new CheckBox[7, 7];
+        private CheckBox[,] _oppositeCheckBoxes = new CheckBox[7, 7];
+
+        // Конвертация: UI-значение (1..7) ↔ внутреннее (-3..3), где "4" = 0 (центр)
+        private int UiToInternal(int uiVal) => uiVal - 4;
+        private int InternalToUi(int internalVal) => internalVal + 4;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Создаём все контролы один раз для макс. количества (7 пластин)
+            CreateAllControlsOnce();
+            // Показываем начальные пластины и эффекты (2 по умолчанию)
+            GeneratePlateControls(2);
+            GenerateEffectControls(2);
+        }
+
+        // Создаёт ВСЕ контролы один раз для макс. количества пластин (7)
+        // Последовательность в panelPlates: Plate 1 (снизу) → Plate 7 (сверху)
+        private void CreateAllControlsOnce()
+        {
+            panelPlates.SuspendLayout();
+            panelEffects.SuspendLayout();
+
+            // ПЛАСТИНЫ: добавляем от 7 к 1, чтобы Plate 1 оказалась внизу
+            for (int i = 6; i >= 0; i--)  // <-- было: for (int i = 0; i < 7; i++)
+            {
+                var platePanel = new FlowLayoutPanel();
+                platePanel.AutoSize = false;
+                platePanel.Size = new System.Drawing.Size(333, 28);
+                platePanel.FlowDirection = FlowDirection.LeftToRight;
+                platePanel.WrapContents = false;
+
+                var plateLabel = new Label
+                {
+                    Text = $"Plate {i + 1}",
+                    AutoSize = true,
+                    Font = new System.Drawing.Font(this.Font, System.Drawing.FontStyle.Bold),
+                    Margin = new Padding(0, 0, 6, 0)
+                };
+                platePanel.Controls.Add(plateLabel);
+
+                string[] posLabels = { "1", "2", "3", "4", "5", "6", "7" };
+                for (int p = 0; p < posLabels.Length; p++)
+                {
+                    RadioButton rb = new RadioButton();
+                    rb.Text = posLabels[p];
+                    rb.AutoSize = true;
+                    rb.Location = new System.Drawing.Point(5 + p * 40, 5);
+                    rb.Tag = i;
+                    if (p == 3) rb.Checked = true;
+                    rb.CheckedChanged += rbPlate_CheckedChanged;
+                    platePanel.Controls.Add(rb);
+                }
+
+                _platePanels[i] = platePanel;
+                panelPlates.Controls.Add(platePanel);
+            }
+
+            // ЭФФЕКТЫ: добавляем строки от Plate 7 к Plate 1
+            for (int src = 6; src >= 0; src--)  // <-- было: for (int src = 0; src < 7; src++)
+            {
+                var row = new TableLayoutPanel();
+                row.AutoSize = true;
+                row.ColumnStyles.Add(new ColumnStyle(SizeType.AutoSize));
+                row.ColumnStyles.Add(new ColumnStyle(SizeType.AutoSize));
+                row.ColumnStyles.Add(new ColumnStyle(SizeType.AutoSize));
+                row.RowStyles.Add(new RowStyle(SizeType.AutoSize));
+
+                var samePanel = new FlowLayoutPanel();
+                samePanel.FlowDirection = FlowDirection.LeftToRight;
+                samePanel.WrapContents = false;
+                samePanel.AutoSize = true;
+                samePanel.Padding = new Padding(0, 0, 4, 0);
+
+                for (int tgt = 0; tgt < 7; tgt++)
+                {
+                    var cb = new CheckBox
+                    {
+                        Text = (tgt + 1).ToString(),
+                        AutoSize = true,
+                        Enabled = (tgt != src),
+                        Tag = new EffectTag { Source = src, Target = tgt, Type = EffectType.Same }
+                    };
+                    _sameCheckBoxes[src, tgt] = cb;
+                    cb.CheckedChanged += EffectCheckBox_CheckedChanged;
+                    samePanel.Controls.Add(cb);
+                }
+
+                var plateLabel = new Label
+                {
+                    Text = $"Plate {src + 1}",
+                    AutoSize = true,
+                    Font = new System.Drawing.Font(this.Font, System.Drawing.FontStyle.Bold),
+                    Margin = new Padding(6, 0, 6, 0)
+                };
+
+                var oppositePanel = new FlowLayoutPanel();
+                oppositePanel.FlowDirection = FlowDirection.LeftToRight;
+                oppositePanel.WrapContents = false;
+                oppositePanel.AutoSize = true;
+                oppositePanel.Padding = new Padding(4, 0, 0, 0);
+
+                for (int tgt = 0; tgt < 7; tgt++)
+                {
+                    var cb = new CheckBox
+                    {
+                        Text = (tgt + 1).ToString(),
+                        AutoSize = true,
+                        Enabled = (tgt != src),
+                        Tag = new EffectTag { Source = src, Target = tgt, Type = EffectType.Opposite }
+                    };
+                    _oppositeCheckBoxes[src, tgt] = cb;
+                    cb.CheckedChanged += EffectCheckBox_CheckedChanged;
+                    oppositePanel.Controls.Add(cb);
+                }
+
+                // ВАЖНО: порядок колонок в TableLayoutPanel должен соответствовать описанию
+                // "слева от надписи Plate N — Opposite, справа — Same"
+                row.Controls.Add(oppositePanel, 0, 0);
+                row.Controls.Add(plateLabel, 1, 0);
+                row.Controls.Add(samePanel, 2, 0);
+
+                _effectRows[src] = row;
+                panelEffects.Controls.Add(row);
+            }
+
+            // Скрываем всё, что за пределами начального значения (2)
+            for (int i = 2; i < 7; i++)
+            {
+                _platePanels[i].Visible = false;
+                _effectRows[i].Visible = false;
+            }
+
+            panelPlates.ResumeLayout();
+            panelEffects.ResumeLayout();
+        }
+
+        private void plateCountCheck_CheckedChanged(object sender, EventArgs e)
+        {
+            // Только один чекбокс может быть активен (как RadioButton)
+            if (sender is CheckBox current && current.Checked)
+            {
+                foreach (Control ctrl in panelPlateCountChecks.Controls)
+                {
+                    if (ctrl is CheckBox chk && chk != current)
+                        chk.Checked = false;
+                }
+            }
+
+            int count = GetSelectedPlateCount();
+            if (count > 0)
+            {
+                GeneratePlateControls(count);
+                GenerateEffectControls(count);
+            }
+        }
+
+        private int GetSelectedPlateCount()
+        {
+            foreach (Control ctrl in panelPlateCountChecks.Controls)
+            {
+                if (ctrl is CheckBox chk && chk.Checked)
+                {
+                    return int.Parse(chk.Tag.ToString());
+                }
+            }
+            return 7; // fallback
+        }
+
+        private void GeneratePlateControls(int plateCount)
+        {
+            panelPlates.SuspendLayout();
+            panelPlates.Controls.Clear();
+
+            // Добавляем панели в обратном порядке, чтобы Plate 1 оказалась внизу
+            for (int i = plateCount - 1; i >= 0; i--)
+            {
+                _platePanels[i].Visible = true; // Явно показываем
+                panelPlates.Controls.Add(_platePanels[i]);
+            }
+            panelPlates.ResumeLayout();
+        }
+
+        private void GenerateEffectControls(int plateCount)
+        {
+            panelEffects.SuspendLayout();
+            panelEffects.Controls.Clear();
+
+            // Добавляем строки в обратном порядке, чтобы Plate 1 оказалась внизу
+            for (int i = plateCount - 1; i >= 0; i--)
+            {
+                _effectRows[i].Visible = true; // Явно показываем
+                panelEffects.Controls.Add(_effectRows[i]);
+            }
+
+            // Внутри каждой видимой строки показываем только чекбоксы для tgt < plateCount
+            for (int src = 0; src < plateCount; src++)
+            {
+                for (int tgt = 0; tgt < 7; tgt++)
+                {
+                    bool show = (tgt < plateCount);
+                    _sameCheckBoxes[src, tgt].Visible = show;
+                    _oppositeCheckBoxes[src, tgt].Visible = show;
+                }
+            }
+            panelEffects.ResumeLayout();
+        }
+
+        private void EffectCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            // Эффекты применяются мгновенно — просто обновляем состояние
+        }
+
+        private void rbPlate_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+
+        private int[] GetStartPositions()
+        {
+            int count = GetSelectedPlateCount();
+            int[] positions = new int[count];
+            // _platePanels[i] соответствует Plate (i+1) напрямую
+            for (int i = 0; i < count; i++)
+            {
+                var panel = _platePanels[i] as FlowLayoutPanel;
+                foreach (Control ctrl in panel.Controls)
+                {
+                    if (ctrl is RadioButton rb && rb.Checked)
+                    {
+                        positions[i] = UiToInternal(int.Parse(rb.Text));
+                        break;
+                    }
+                }
+            }
+            return positions;
+        }
+
+        private void ReadEffects(LockConfig config)
+        {
+            // Сбрасываем все в None
+            for (int src = 0; src < config.PlateCount; src++)
+            {
+                for (int tgt = 0; tgt < config.PlateCount; tgt++)
+                {
+                    config.Effects[src, tgt] = EffectType.None;
+                }
+            }
+
+            // Считываем напрямую из кэша _effectRows по индексу источника
+            for (int src = 0; src < config.PlateCount; src++)
+            {
+                var row = _effectRows[src];
+                foreach (Control child in row.Controls)
+                {
+                    if (child is FlowLayoutPanel fp)
+                    {
+                        foreach (Control cb in fp.Controls)
+                        {
+                            if (cb is CheckBox check && check.Tag is EffectTag tag)
+                            {
+                                if (check.Checked)
+                                    config.Effects[tag.Source, tag.Target] = tag.Type;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void SetEffectsFromConfig(LockConfig config)
+        {
+            for (int src = 0; src < config.PlateCount; src++)
+            {
+                var row = _effectRows[src];
+                foreach (Control child in row.Controls)
+                {
+                    if (child is FlowLayoutPanel fp)
+                    {
+                        foreach (Control cb in fp.Controls)
+                        {
+                            if (cb is CheckBox check && check.Tag is EffectTag tag)
+                            {
+                                check.Checked = (config.Effects[tag.Source, tag.Target] == tag.Type);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void buttonSolve_Click(object sender, EventArgs e)
+        {
+            int count = GetSelectedPlateCount();
+            var config = new LockConfig
+            {
+                PlateCount = count,
+                StartPositions = GetStartPositions(),
+                Effects = new EffectType[count, count]
+            };
+
+            ReadEffects(config);
+
+            var solver = new LockpickSolver();
+            var result = solver.Solve(config);
+            textBoxResult.Text = solver.FormatSolution(result, "Результат");
+        }
+    }
+
     public enum EffectType { None, Same, Opposite }
     public enum Direction { Left, Right }
 
@@ -232,5 +552,13 @@ namespace Gothic_Remake_Breaker
 
             return sb.ToString();
         }
+    }
+
+    // Тег для чекбоксов эффектов
+    internal class EffectTag
+    {
+        public int Source;   // пластина-источник (0-based)
+        public int Target;   // пластина-цель (0-based)
+        public EffectType Type; // Same или Opposite
     }
 }
